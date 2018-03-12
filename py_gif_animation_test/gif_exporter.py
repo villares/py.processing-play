fee6612f8b83d7e4d988e1853452e4b69c202b67
Alexandre B A Villares <abav@lugaralgum.com>
2018-03-12 07:17:54 -0300

    comments & cleanup

--- py_gif_animation_test/gif_exporter.py
+++ py_gif_animation_test/gif_exporter.py
@@ -1,17 +1,33 @@
-def gif_export(GifMaker, filename="exported", frames=0, repeat=0, quality=255, delay=17):
-    """
-    Be careful! frames=0 will stop the sketch on keyPressed or frameCount >= 100000... 
-    """
-    if frameCount == 1:
+"""
+Alexandre B A Villares http://abav.lugaralgum.com - GPL v3 
+
+A helper for the Processing gifAnimation library (https://github.com/jordanorelli)
+ported to Processing 3 by 01010101 (https://github.com/01010101)
+Download the library from https://github.com/01010101/GifAnimation/archive/master.zip
+This helper was inspired by an example by Art Simon https://github.com/APCSPrinciples/AnimatedGIF/
+
+Put add_library('gifAnimation') at the start of your sketch
+and add gif_export(GifMaker) at the end of draw()
+"""
+def gif_export(GifMaker,             # gets a reference to the library
+               filename="exported",  # .gif will be added
+               repeat=0,             # 0 makes it an "endless" animation
+               quality=255,          # quality range 0 - 255
+               delay=170,            # this is quick
+               frames=0):            # 0 will stop on keyPressed or frameCount >= 100000
+    try:
+        gifExporter
+    except NameError:
         global gifExporter
         gifExporter = GifMaker(this, filename + ".gif")
-        gifExporter.setRepeat(repeat)  # 0 makes it an "endless" animation
-        gifExporter.setQuality(quality)  # quality range 0 - 255
+        gifExporter.setRepeat(repeat)
+        gifExporter.setQuality(quality)
         gifExporter.setDelay(delay)
-    gifExporter.addFrame()
         
-    if (frames == 0 and keyPressed or frameCount >= 100000) or (frames != 0 and frameCount >= frames):
+    gifExporter.addFrame()
+
+    if (frames == 0 and keyPressed or frameCount >= 100000) \
+            or (frames != 0 and frameCount >= frames):
         gifExporter.finish()
         print("gif saved")
         exit()
- 
--- py_gif_animation_test/py_gif_animation_test.pyde
+++ py_gif_animation_test/py_gif_animation_test.pyde
@@ -1,5 +1,5 @@
 """
-A helper for the Processing GifMaker library (https://github.com/jordanorelli)
+Testing a helper for the Processing GifMaker library (https://github.com/jordanorelli)
 ported to Processing 3 by 01010101 (https://github.com/01010101)
 Download it from https://github.com/01010101/GifAnimation/archive/master.zip
 Inspired by example by Art Simon https://github.com/APCSPrinciples/AnimatedGIF/
@@ -10,13 +10,12 @@ from gif_exporter import gif_export
 
 diameter = 1
 def setup():
-    size(200,200)
+    size(200, 200)
     smooth()
-    
+
 def draw():
     global diameter
-    ellipse(50,50,diameter,diameter)
+    ellipse(50, 50, diameter, diameter)
     diameter = diameter + 1
     print(frameCount)
     gif_export(GifMaker, frames=250)
-
