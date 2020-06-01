880ef9d94dfc50a1e18caf9c72bf2f4dba5fbaa6
Alexandre B A Villares <abav@lugaralgum.com>
2020-05-31 22:26:20 -0300

    Update gif_export.py

--- py_gif_animation_test/gif_export.py
+++ py_gif_animation_test/gif_export.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 
 """
-Alexandre B A Villares http://abav.lugaralgum.com - GPL v3 
+Alexandre B A Villares http://abav.lugaralgum.com - v2020_05_31 - Licensed under GPL v3
+Inspired by an example by Art Simon https://github.com/APCSPrinciples/AnimatedGIF/
 
-This is a helper for the Processing gifAnimation library
-https://github.com/extrapixel/gif-animation/tree/3.0
-This helper was inspired by an example by Art Simon
-https://github.com/APCSPrinciples/AnimatedGIF/
-This should be saved as a 'tab' named gif_export.py in your Processing Python Mode sketch
-
+This is a helper for the Processing gifAnimation library https://github.com/extrapixel/gif-animation/tree/3.0
 Download the library from github.com/extrapixel/gif-animation/archive/3.0.zip 
 Unzip and copy the gifAnimation folder into your libraries folder, like shown below:
 user/sketchbook/libraries/gifAnimation (Linux) 
 user/Documents/Processing/libraries/gifAnimation (Mac/Windows) 
 
+# This file should be saved as a 'tab' named gif_export.py in your Processing Python Mode sketch
 # Restart the IDE and add these lines at the start of your sketch:
 add_library('gifAnimation')
 from gif_export import gif_export
+
 # then add this at the end of draw():
 gif_export(GifMaker, "filename")
+
 """
 
 def gif_export(GifMaker,             # gets a reference to the library
