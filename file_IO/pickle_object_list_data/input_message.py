83d585bc39bb1dfbfab43433d897a40b0feeb601
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2021-02-19 19:51:20 -0300

    Update input_message.py

--- file_IO/pickle_object_list_data/input_message.py
+++ file_IO/pickle_object_list_data/input_message.py
@@ -1,3 +1,3 @@
 def input(message='', default=''):
     from javax.swing import JOptionPane
-    return JOptionPane.showInputDialog(None, message, default='')
+    return JOptionPane.showInputDialog(None, message, default)
